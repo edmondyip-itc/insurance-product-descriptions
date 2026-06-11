6ca13cf2e56aa1fe4bcb3851317ab9dbc5c72ce4
edmondyip-itc <edmond.yip@itccompliance.co.uk>
2026-06-11 10:30:30 +0100

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/insurance-product-descriptions/' : '/',
+    baseURL: '/',
     buildId: `build-${Date.now()}`,
     head: {
       title: 'Insurance Product Descriptions'
