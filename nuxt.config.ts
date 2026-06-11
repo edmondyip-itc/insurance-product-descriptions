9f5417c240e32ec320299f5acb2d19abed9df7f6
edmondyip-itc <edmond.yip@itccompliance.co.uk>
2026-06-11 09:52:41 +0100

    update setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,13 @@
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   app: {
-    baseURL: '/insurance-product-descriptions/',
+    baseURL: '/',
     head: {
       title: 'Insurance Product Descriptions'
     }
   },
   nitro: {
     preset: 'static',
-    prerender: {
-      crawlLinks: true,
-      routes: ['/sitemap.xml', '/robots.txt']
-    },
     outDir: 'docs'
   }
 })
